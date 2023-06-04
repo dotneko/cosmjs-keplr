176845048296228d4995179d8ab67fb512346a46
dotneko <4761762+dotneko@users.noreply.github.com>
2023-06-04 00:22:43 +0000

    Get address and balance then submit transaction

--- components/FaucetSender.tsx
+++ components/FaucetSender.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { Coin, StargateClient } from "@cosmjs/stargate"
+import { Coin, SigningStargateClient, StargateClient } from "@cosmjs/stargate"
+import { AccountData, OfflineSigner } from "@cosmjs/proto-signing"
 import { ChainInfo, Window as KeplerWindow } from "@keplr-wallet/types"
 import { ChangeEvent, Component, MouseEvent } from "react"
 import styles from '../styles/Home.module.css'
@@ -76,6 +77,45 @@ export class FaucetSender extends Component<FaucetSenderProps, FaucetSenderState
 
         // Suggest the testnet chain to Keplr
         await keplr.experimentalSuggestChain(this.getTestnetChainInfo())
+
+        // Create the signing client
+    const offlineSigner = window.getOfflineSigner!("theta-testnet-001")
+    const signingClient = await SigningStargateClient.connectWithSigner(
+        rpcUrl,
+        offlineSigner,
+    )
+    // Get the address and balance of your user
+    const account: AccountData = (await offlineSigner.getAccounts())[0]
+    this.setState({
+        myAddress: account.address,
+        myBalance: (await signingClient.getBalance(account.address, denom))
+            .amount,
+    })
+    // Submit the transaction to send tokens to the faucet
+    const sendResult = await signingClient.sendTokens(
+        account.address,
+        faucetAddress,
+        [
+            {
+                denom: denom,
+                amount: toSend,
+            },
+        ],
+        {
+            amount: [{ denom: "uatom", amount: "500" }],
+            gas: "200000",
+        },
+    )
+    // Print the result to the console
+    console.log(sendResult)
+    // Update the balance in the user interface
+    this.setState({
+        myBalance: (await signingClient.getBalance(account.address, denom))
+            .amount,
+        faucetBalance: (
+            await signingClient.getBalance(faucetAddress, denom)
+        ).amount,
+    })
     }
 
     // Get Cosmos Hub Testnet chain parameters
